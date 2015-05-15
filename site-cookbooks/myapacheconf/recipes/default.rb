99957bd784cb401d64de86e68bb0915a408a335a
DQNEO <dqneoo@gmail.com>
2015-05-15 18:01:29 +0900

    put hello.php for test

--- site-cookbooks/myapacheconf/recipes/default.rb
+++ site-cookbooks/myapacheconf/recipes/default.rb
@@ -1,5 +1,14 @@
 include_recipe 'apache2'
 
+directory "/srv/myapp" do
+  owner "vagrant"
+end
+
+file "/srv/myapp/hello.php" do
+  owner "vagrant"
+  content "<?php echo 'hello php version' . PHP_VERSION;"
+end
+
 web_app 'myapp' do
   template 'myapp.conf.erb'
   server_name 'vagrantphp'
